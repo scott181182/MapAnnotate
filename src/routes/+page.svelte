a40a183c5a92b9ef964caed1403bc370465e774f
Scott Fasone <scott181182@yahoo.com>
2023-09-26 18:54:52 -0400

    feat: added classes for data structures with better selection logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,113 @@
 <script lang="ts">
-import L, { Map as LeafletMap } from 'leaflet';
+import L, { Map as LeafletMap, type LatLngLiteral, type LatLngExpression } from 'leaflet';
 import "leaflet/dist/leaflet.css";
+import _ from "lodash";
+import { v4 as uuid4 } from "uuid";
 
 import { BlueCrossPointIconMap, RedCrossPointIconMap } from '$lib/map';
 
 
 
+interface MapNodeData {
+    id: string;
+}
+interface MapEdgeData {
+    fromNodeId: string;
+    toNodeId: string;
+}
+
+interface Selectable {
+    select(): void;
+    deselect(): void;
+}
+
+class MapMarker extends L.Marker implements Selectable {
+    public constructor(
+        latlng: LatLngExpression,
+        public readonly node: MapNodeData
+    ) {
+        super(latlng);
+    }
+
+    public select() {
+        if(currentSelection) { currentSelection.data.deselect(); }
+        const zoom = this._map.getZoom() ?? 1;
+        this.setIcon(RedCrossPointIconMap.get(zoom))
+        currentSelection = { type: "node", data: this };
+    }
+    public deselect() {
+        const zoom = this._map.getZoom() ?? 1;
+        this.setIcon(BlueCrossPointIconMap.get(zoom))
+        currentSelection = null;
+    }
+}
+class MapLine extends L.Polyline implements Selectable {
+    public static readonly DEFAULT_COLOR = "#3a8ece";
+    public static readonly ACTIVE_COLOR = "#f00";
+
+    public readonly edge: MapEdgeData;
+
+    public constructor(
+        from: MapMarker,
+        to: MapMarker,
+    ) {
+        super([ from.getLatLng(), to.getLatLng() ], {
+            color: MapLine.DEFAULT_COLOR,
+            bubblingMouseEvents: false
+        });
+        this.edge = {
+            fromNodeId: from.node.id,
+            toNodeId: to.node.id
+        };
+    }
+
+    public select() {
+        if(currentSelection) { currentSelection.data.deselect(); }
+        this.setStyle({ color: MapLine.ACTIVE_COLOR });
+        currentSelection = { type: "edge", data: this };
+    }
+    public deselect() {
+        this.setStyle({ color: MapLine.DEFAULT_COLOR });
+        currentSelection = null;
+    }
+}
+
+
+
 let map: LeafletMap | null;
+const initialView: [number, number] = [39.1319066,-84.5148446];
 
 const nodeLayer = L.layerGroup();
-let selectedNode: L.Marker | null = null;
-
 const edgeLayer = L.layerGroup();
-let selectedEdge: L.Polyline | null = null;
 
-const initialView: [number, number] = [39.1319066,-84.5148446];
+type CurrentSelection = { type: "node", data: MapMarker } | { type: "edge", data: MapLine };
+let currentSelection: CurrentSelection | null = null;
+
+
+
+function addNodeMarker(latlng: L.LatLngExpression) {
+    const mark = new MapMarker(latlng, { id: uuid4() })
+    mark.setIcon(RedCrossPointIconMap.get(map?.getZoom() ?? 1));
+    mark.on("click", (ev) => {
+        if(ev.originalEvent.shiftKey && currentSelection?.type === "node") {
+            addNodeEdge(currentSelection.data, mark);
+        }
+        mark.select();
+    });
+    nodeLayer.addLayer(mark);
+    mark.select();
+    return mark;
+}
+function addNodeEdge(from: MapMarker, to: MapMarker) {
+    console.log("New Edge!");
+    const line = new MapLine(from, to);
+    line.on("click", () => {
+        line.select();
+    });
+    edgeLayer.addLayer(line);
+    console.log(edgeLayer);
+}
+
 
 
 
@@ -35,8 +128,8 @@ function createMap(container: HTMLElement) {
 
     m.on("zoomend", () => {
         nodeLayer.eachLayer((l) => {
-            const mark = l as L.Marker;
-            if(mark === selectedNode) {
+            const mark = l as MapMarker;
+            if(currentSelection?.data === mark) {
                 mark.setIcon(RedCrossPointIconMap.get(m.getZoom()))
             } else {
                 mark.setIcon(BlueCrossPointIconMap.get(m.getZoom()))
@@ -45,62 +138,16 @@ function createMap(container: HTMLElement) {
     })
 
     m.on("click", (ev) => {
-        if(ev.originalEvent.shiftKey && selectedNode) {
-            addNodeEdge(selectedNode.getLatLng(), ev.latlng);
+        const previousMark = currentSelection;
+        const newMark = addNodeMarker(ev.latlng);
+        if(ev.originalEvent.shiftKey && previousMark?.type === "node") {
+            addNodeEdge(previousMark.data, newMark);
         }
-        addNodeMarker(ev.latlng);
     });
     return m;
 }
 
-function deselectNodes() {
-    const zoom = map?.getZoom() ?? 1;
-    if(selectedNode) {
-        selectedNode.setIcon(BlueCrossPointIconMap.get(zoom))
-    }
-    selectedNode = null;
-}
-function selectNode(mark: L.Marker) {
-    deselectNodes();
-    deselectEdges();
-    const zoom = map?.getZoom() ?? 1;
-    selectedNode = mark;
-    mark.setIcon(RedCrossPointIconMap.get(zoom));
-}
-function deselectEdges() {
-    const zoom = map?.getZoom() ?? 1;
-    if(selectedEdge) {
-        selectedEdge.setStyle({ color: "#3a8ece" })
-    }
-    selectedEdge = null;
-}
-function selectEdge(line: L.Polyline) {
-    deselectNodes();
-    deselectEdges();
-    selectedEdge = line;
-    line.setStyle({ color: "#f00" });
-}
 
-function addNodeMarker(latlng: L.LatLngExpression) {
-    const mark = L.marker(latlng, {
-        icon: RedCrossPointIconMap.get(map?.getZoom() ?? 1)
-    });
-    mark.on("click", (ev) => {
-        if(ev.originalEvent.shiftKey && selectedNode) {
-            addNodeEdge(selectedNode.getLatLng(), mark.getLatLng());
-        }
-        selectNode(mark);
-    });
-    nodeLayer.addLayer(mark);
-    selectNode(mark);
-}
-function addNodeEdge(from: L.LatLngExpression, to: L.LatLngExpression) {
-    const line = L.polyline([ from, to ], {
-        color: "#3a8ece"
-    });
-    line.on("click", () => { selectEdge(line); });
-    edgeLayer.addLayer(line);
-}
 
 function mapAction(container: HTMLElement) {
     map = createMap(container);
@@ -116,8 +163,15 @@ function mapAction(container: HTMLElement) {
 function resizeMap() {
     if(map) { map.invalidateSize(); }
 }
+
+
+
+
+$: asideTitle = currentSelection ? _.startCase(currentSelection.type) : "no selection";
 </script>
 <svelte:window on:resize={resizeMap}/>
+
+
 
 <style>
 #map {
@@ -131,6 +185,10 @@ function resizeMap() {
 </style>
 
 
+
+<aside>
+    <h1>{asideTitle}</h1>
+</aside>
 <section class="container h-100">
     <div id="map" use:mapAction></div>
 </section>
