343858b6bfa879f8a656938eff0211eeb248bf9b
Scott Fasone <scott181182@yahoo.com>
2023-10-12 18:08:33 -0400

    feat(client): better layout and icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
-<header id="page-header" class="navbar navbar-dark bg-dark px-4">
-    <span class="navbar-brand mb-0 h1">Map Annotate</span>
-</header>
-<main style="flex-grow: 1">
-    <slot></slot>
-</main>
+<script lang="ts">
+import "../app.css";
+</script>
+
+<div class="flex flex-col h-screen">
+    <header id="page-header" class="navbar navbar-dark bg-dark px-4">
+        <span class="navbar-brand mb-0 h1">Map Annotate</span>
+    </header>
+    <main style="flex-grow: 1">
+        <slot></slot>
+    </main>
+</div>
